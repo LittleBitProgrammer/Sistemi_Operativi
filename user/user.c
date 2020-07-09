c86514860892685ed1018c42ea67a67a40c40900
Roberto Vecchio <vecchioroberto93@gmail.com>
2020-07-09 12:28:17 +0200

    ID
    - added C file to take userID and groupUserID

---
+++ user/user.c
@@ -0,0 +1,22 @@
+/*
+* Nome: user.c
+* Scopo: Mostrare lo userID e il GroupUserID
+* Autore: Roberto Vecchio
+* Data: 09 Luglio 2020
+*/
+
+// Directive
+#include "apue.h" // to use Operating System function
+
+int main(void){
+    // Instructions
+
+    /*
+    * getuid() return the userID
+    * getgid() return the groupUserID
+    */
+    printf("uid = %d, gid = %d", getuid(), getgid());
+
+    // Operating System Code
+    return 0;
+}
