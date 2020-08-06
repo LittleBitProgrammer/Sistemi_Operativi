aa6d6b319bba5d35f7ae5d300df7c859cdc4d748
Roberto <vecchioroberto93@gmail.com>
2020-08-06 18:29:15 +0200

    fix
    - minor fix

--- 07_lseek/seek.c
+++ 07_lseek/seek.c
@@ -1,13 +1,15 @@
-/*
-* Nome: seek.c
-* Scopo: stampare a video se l'operazione lseek è andata a buon fine o meno e se viene eseguita con successo allegare in output
-*        l'offset corrente
-* Data: 06 Agosto 2020
-* Autore: Roberto Vecchio ©
-* build: gcc -Wall -o seek seek.c
-* execution_test_one: ./seek < /etc/motd
-* 
-*/
+/* ---------------------------------------------------------------------------------------------------------------------------
+* |Nome:                 |seek.c                                                                                             |
+* |Scopo:                |stampare a video se l'operazione lseek è andata a buon fine o meno e se viene eseguita con successo|
+* |                      |allegare in output l'offset corrente                                                               |
+* |Data:                 |06 Agosto 2020                                                                                     |
+* |Autore:               |Roberto Vecchio ©                                                                                  |
+* |build:                |gcc -Wall -o seek seek.c                                                                           |
+* |execution_test_one:   |./seek < /etc/motd                                                                                 |
+* |execution_test_two:   |cat < /etc/motd | ./seek                                                                           |
+* |execution_test_three: |./seek < /var/spool/cron/FIFO                                                                      |
+  ---------------------------------------------------------------------------------------------------------------------------*/
+
 // DIRECTIVE
 #include <stdio.h>  // per usare printf
 #include <unistd.h> // per usare lseek e il tipo off_t
@@ -34,3 +36,13 @@ int main(void){
     // OS code
     return 0;
 }
+
+// ? Test Build One:
+// * Viene eseguito con successo, settando come standartd input /etc/motd
+
+// ? Test Build Two:
+// * Fallisce, prima concatena nello standard output lo standard inout /etc/motd e poi utilizza una pipe con il nostro eseguibile
+// * ma un pipe con lseek ritorna -1
+
+// ? Test Build Three:
+// * Fallisce viene utilizzata una Fifo come argomento della funzione lseek
